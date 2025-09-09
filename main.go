19a7266bb99130f7b808ea8353dff791460390f8
Andres Court <andres@andrescourt.com>
2025-09-08 22:15:23 -0500

    use go func to read the file

--- main.go
+++ main.go
@@ -15,40 +15,48 @@ func main() {
 		slog.Error("Unable to open the file", "err", err)
 		os.Exit(1)
 	}
-	defer func() {
-		err := file.Close()
-		if err != nil {
-			slog.Error("Unable to close the file", "err", err)
-			os.Exit(1)
 
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read line: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			buf := make([]byte, 8)
+			n, err := f.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+
+				slog.Error("Unable to read the file", "err", err)
+				os.Exit(1)
+			}
+
+			buf = buf[:n]
+			if idx := bytes.IndexByte(buf, '\n'); idx != -1 {
+				str += string(buf[:idx])
+				buf = buf[idx+1:]
+				out <- str
+				str = ""
+			}
+			str += string(buf)
+
+		}
+
+		if len(str) > 0 {
+			out <- str
 		}
 	}()
 
-	str := ""
-	for {
-		buf := make([]byte, 8)
-		n, err := file.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
-			slog.Error("Unable to read the file", "err", err)
-			os.Exit(1)
-		}
-
-		buf = buf[:n]
-		if idx := bytes.IndexByte(buf, '\n'); idx != -1 {
-			str += string(buf[:idx])
-			buf = buf[idx+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(buf)
-
-	}
-
-	if len(str) > 0 {
-		fmt.Printf("read: %s\n", str)
-	}
+	return out
 }
