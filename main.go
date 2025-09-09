6625d1e07fc3367b531c3e77ce4b63ef417b124f
Andres Court <andres@andrescourt.com>
2025-09-08 21:41:30 -0500

    read data from a file 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+)
 
 func main() {
-	fmt.Printf("I hope I get the job")
+	file, err := os.Open("messages.txt")
+
+	if err != nil {
+		slog.Error("Unable to open the file", "err", err)
+		os.Exit(1)
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			slog.Error("Unable to close the file", "err", err)
+			os.Exit(1)
+
+		}
+	}()
+
+	for {
+		buf := make([]byte, 8)
+		n, err := file.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			}
+
+			slog.Error("Unable to read the file", "err", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("read: %s\n", string(buf[:n]))
+	}
 }
